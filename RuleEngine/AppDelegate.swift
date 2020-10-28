c474e040774e962a901051f7aac6e33371a9d69a
uttam028 <uttam028@gmail.com>
2020-10-28 14:55:49 -0400

    Keep alive using location service

--- RuleEngine/AppDelegate.swift
+++ RuleEngine/AppDelegate.swift
@@ -20,7 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        KeepAliveByLocationManager.sharedInstance.startLocationToKeepAlive()
         RuleEngine.sharedInstance.start()
+        
         return true
     }
 
